b79c8a51c63b0ad7ad3349be6b280a9081c1fc8c
anirudshrestha <ANIRUDSHRESTHA@boisestate.edu>
2023-10-09 20:36:35 -0600

    fix: primary secondary color issue and add food page

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -11,6 +11,10 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'food',
+    loadChildren: () => import('./food/food.module').then( m => m.FoodPageModule)
+  },
 ];
 
 @NgModule({
