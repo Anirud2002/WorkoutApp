3acc2daef4e2360bf1f4564e464dfa671c27e2f8
anirudshrestha <ANIRUDSHRESTHA@boisestate.edu>
2023-10-24 21:13:20 -0600

    add: tabs for navigation

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -7,10 +7,11 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { NgChartsModule } from 'ng2-charts';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, NgChartsModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, SharedModule, NgChartsModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
